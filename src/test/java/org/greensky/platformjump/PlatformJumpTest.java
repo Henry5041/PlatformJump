468893226063cec2c56e816e500d1996c24f8318
Henry5041 <henry5041@126.com>
2014-11-13 18:27:02 +0800

    Create PlatformJumpTest.java
    The test class

---
+++ src/test/java/org/greensky/platformjump/PlatformJumpTest.java
@@ -0,0 +1,19 @@
+package org.greensky.platformjump;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlatformJumpTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testOnEnable() {
+		
+	}
+
+}
